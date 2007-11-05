31696b82d4dcec8024b3bda0e22b96568aa9e0c5
ferranb <ferranb@f650fdd5-be3d-0410-89c6-9529df1252a1>
2007-11-05 00:55:13 +0000

    Add ExecuteFile command
    git-svn-id: svn://svn.code.sf.net/p/naturalcli/code/trunk@19 f650fdd5-be3d-0410-89c6-9529df1252a1

---
+++ naturalcli/src/org/naturalcli/commands/ExecuteFile.java
@@ -0,0 +1,38 @@
+/* 
+ * ExecuteFile.java
+ *
+ * Copyright (C) 2007 Ferran Busquets
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package org.naturalcli.commands;
+
+import java.util.Set;
+
+/**
+ * @author Ferran Busquets
+ *
+ */
+public class ExecuteFile  extends Command {
+	
+	public ExecuteFile(NaturalCLI naturalCLI) 
+	{
+		super("execute file <filename:string>", "Execute the commands on file.",
+				new ExecuteFileCommandExecutor(naturalCLI)
+		);
+	}	
+
+}
+
---
+++ naturalcli/src/org/naturalcli/commands/ExecuteFileCommandExecutor.java
@@ -0,0 +1,55 @@
+/* 
+ * HelpCommandExecutor.java
+ *
+ * Copyright (C) 2007 Ferran Busquets
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package org.naturalcli.commands;
+
+
+import java.io.*;
+
+import org.naturalcli.NaturalCLI;
+import org.naturalcli.commands.ICommandExecutor;
+
+
+/**
+ * @author Ferran Busquets
+ *
+ */
+public class ExecuteFileCommandExecutor implements ICommandExecutor {
+
+	private NaturalCLI naturalCLI;
+	
+	public ExecuteFileCommandExecutor(NaturalCLI naturalCLI)
+	{
+		this.naturalCLI = naturalCLI;
+	}
+		
+	/* (non-Javadoc)
+	 * @see org.naturalcli.ICommandExecutor#execute(java.lang.Object[])
+	 */
+	@Override
+	public void execute(Object[] params) throws Exception {
+		String file_name = params[0].toString();
+        BufferedReader in = new BufferedReader(new FileReader(file_name));
+        String command;
+        while ((command = in.readLine()) != null)
+    		this.naturalCLI.execute(command, 0);
+        in.close();
+	}
+
+}
